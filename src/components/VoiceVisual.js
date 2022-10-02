440d14e602bc85c03ba5c98a04102aa9f1ae6206
shalawkarim <shalaw@glaciermediadigital.ca>
2022-10-01 22:29:53 -0700

    changed icon and title

--- src/components/VoiceVisual.js
+++ src/components/VoiceVisual.js
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 import { SpectrumVisualizer, SpectrumVisualizerTheme } from 'react-audio-visualizers';
-import { AiOutlineStock} from 'react-icons/ai';
 import florida_low_song from '../assets/flow-rida-low.mp3';
+import { MdOutlineShowChart} from 'react-icons/md';
 import {ImStatsBars} from 'react-icons/im';
 import {TbAntennaBars5} from 'react-icons/tb';
-import {GiRadialBalance} from 'react-icons/gi';
-import {GrRadial} from 'react-icons/gr';
+import { GiCircle} from 'react-icons/gi';
+import {TbCircleDotted} from 'react-icons/tb';
 
 const VoiceVisual = () => {
     const [vis, setVis] = useState(SpectrumVisualizerTheme.radialSquaredBars)
@@ -23,11 +23,11 @@ const VoiceVisual = () => {
     />
     </div>
     <div className='flex items-center justify-around mx-12 text-2xl'>
-        <AiOutlineStock className='cursor-pointer hover:text-[#6F45FF] duration-500' onClick={() => setVis(SpectrumVisualizerTheme.line)} />
-        <TbAntennaBars5 className='cursor-pointer hover:text-[#6F45FF] duration-500' onClick={() => setVis(SpectrumVisualizerTheme.roundBars)} />
-        <ImStatsBars className='cursor-pointer hover:text-[#6F45FF] duration-500' onClick={() => setVis(SpectrumVisualizerTheme.squaredBars)} />
-        <GiRadialBalance className='cursor-pointer hover:text-[#6F45FF] duration-500' onClick={() => setVis(SpectrumVisualizerTheme.radialSquaredBars)} />
-        <GrRadial className='cursor-pointer hover:text-[#6F45FF] duration-500' onClick={() => setVis(SpectrumVisualizerTheme.radialLine)} />
+        <MdOutlineShowChart className='cursor-pointer text-indigo-400 hover:text-indigo-800 duration-500' onClick={() => setVis(SpectrumVisualizerTheme.line)} />
+        <TbAntennaBars5 className='cursor-pointer text-indigo-400 hover:text-indigo-800 duration-500' onClick={() => setVis(SpectrumVisualizerTheme.roundBars)} />
+        <ImStatsBars className='cursor-pointer text-indigo-400 hover:text-indigo-800 duration-500' onClick={() => setVis(SpectrumVisualizerTheme.squaredBars)} />
+        <TbCircleDotted className='cursor-pointer text-indigo-400 hover:text-indigo-800 duration-500' onClick={() => setVis(SpectrumVisualizerTheme.radialSquaredBars)} />
+        <GiCircle className='cursor-pointer text-indigo-400 hover:text-indigo-800 duration-500' onClick={() => setVis(SpectrumVisualizerTheme.radialLine)} />
     </div>
     </>
   )
